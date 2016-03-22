a0e36bfa59d9b10e4a36a2d8c603026dae1fe4f1
Gonzalo Pincheira A <g.pincheira.a@gmail.com>
2016-03-22 17:15:53 -0300

    [feature]: Primera version

---
+++ angular-rut-validator.js
@@ -0,0 +1,48 @@
+(function(window, angular, undefined){
+
+  'use strict';
+
+  angular
+    .module('gpDirectives', [])
+    .directive('gpRutValidator', directive);
+  
+  directive.$inject = ['$compile'];
+  function directive($compile){
+    var ddo = {
+      restrict: 'A',
+      require: 'ngModel',
+      link: linkFn
+    };
+    return ddo;
+    
+    function linkFn(scope, element, attrs, ngModel){
+      ngModel.$validators.rutInvalid = function(rut) {
+        if(rut){
+          rut = rut.match(/[0-9Kk]+/g).join('');
+          return verificarRut(rut);
+        }
+      };
+    }
+    
+    function verificarRut(rut){
+      var factor = 2,
+          auxSum = 0,
+          digitoVerificador = rut[rut.length - 1],
+          l = rut.length - 1,
+          digitoCalculado;
+
+      while(l--){
+        //+ operador 'unary' para hacer cast a number
+        auxSum += +(rut[l]) * factor;
+        factor = factor === 7 ? 2
+                              : ++factor; //++ a la izquierda
+      }
+
+      digitoCalculado = 11 - (auxSum % 11);
+      digitoCalculado =  digitoCalculado === 11 ? 0
+                                                : (digitoCalculado === 10? 'K' : digitoCalculado);
+      return String(digitoCalculado).toLowerCase() === String(digitoVerificador).toLowerCase();
+    }
+  }
+
+})(window, angular);
